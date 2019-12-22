8be18ca76c4c039578fa3d2de5cc731211654780
sweing <sjweingaertner@gmail.com>
2019-12-23 00:29:34 +0100

    change poi source - requesting permanent fix

--- main.js
+++ main.js
@@ -192,8 +192,8 @@ function refreshMarkers() {
     if ($('.decarbnowpopup').length > 0) {
         return;
     }
-    //$.get('https://decarbnow.space/api/poi', function(data) {
-    $.get('poi.json', function(data) {
+    $.get('https://decarbnow.space/api/poi', function(data) {
+    //$.get('poi.json', function(data) {
         console.log("function refreshMarkers");
         for (var i in currentMarkers) {
             for (var mi in currentMarkers[i]) {

9dbd70ab50a7f153cb061869f428cf1726b0dcbe
sweing <sjweingaertner@gmail.com>
2020-04-13 23:39:40 +0200

    copyright label new map

--- main.js
+++ main.js
@@ -204,7 +204,8 @@ function createBackgroundMapLight() {
     return tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}.png', {
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3'],
-        attribution: '© <a href="https://maps.google.com">Google Maps</a>, '+ 
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, '+
+        			 '© <a href="https://carto.com/attribution">CARTO</a>, '+
                      '<a href="https://disc.gsfc.nasa.gov/datasets/OMNO2d_003/summary?keywords=omi">NASA</a>, '+
                      '<a href="https://earth.esa.int/web/guest/missions/esa-eo-missions/sentinel-5p">ESA/Copernicus</a>, '+
                      '<a href="https://github.com/wri/global-power-plant-database">WRI</a>'

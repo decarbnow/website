8a9c6c916efe0011ab775172b24a2d49ae711924
petres <peter.reschenhofer@gmail.com>
2020-04-27 16:09:22 +0200

    split up dev and prod, add html-loader for img in html

---
+++ webpack.config.base.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+
+//const path = require('path');
+
+module.exports = {
+    entry: {
+        map: './js/map.js',
+        start: './js/start.js',
+        mapStyle: './style/map.scss',
+        startStyle: './style/start.scss',
+    },
+    module: {
+        rules: [{
+            test: /\.html$/i,
+            loader: 'html-loader',
+        }, {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        }, {
+            test: /\.(png|jpe?g|gif|ttf|woff|woff2|eot|svg)$/i,
+            use: ['file-loader']
+        }, {
+            test: /\.scss$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', "sass-loader"],
+        }],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin([
+            { from: 'data', to: 'data' }
+        ]),
+        new MiniCssExtractPlugin({
+            filename: '[name].[chunkhash].css'
+        }),
+        new FixStyleOnlyEntriesPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'map/index.html',
+            chunks: ['map', 'mapStyle'],
+            template: 'map.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['start', 'startStyle'],
+            template: 'start.html'
+        }),
+    ],
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,9 @@
+const baseConfig = require('./webpack.config.base.js');
+const merge = require('webpack-merge');
+
+module.exports = merge(baseConfig, {
+    mode: 'production',
+    output: {
+        filename: '[name].[hash].js'
+    }
+});

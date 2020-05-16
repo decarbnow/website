f8b063086977f62ea4fc50abdd0fa1a285600c6f
petres <peter.reschenhofer@gmail.com>
2020-05-16 18:07:50 +0200

    add bar to story tweets

--- js/map/tweets.js
+++ js/map/tweets.js
@@ -32,30 +32,38 @@ let tweets = {
                 lng: t.longitude,
             }
 
-            let ids = [id];
-            if (info.story && tweetList.stories[info.story])
-                ids = tweetList.stories[info.story];
-
             L.marker(latlng, {icon: icons[info.tags[0]]})
                 .addTo(tweets.clusters)
                 .on('click', function () {
                     if (window.twttr.widgets) {
+                        let ids = [id];
+                        if (info.story)
+                            ids = tweetList.stories[info.story];
 
-                        let text = ids.map(tweetId => {
-                            return `<div class="tweet" data-tweet="${tweetId}"></div>`;
-                        }).join('')
+                        let entries = ids.map(tweetId => {
+                            let classes = ['tweet'];
+                            if (id == tweetId)
+                                classes.push('selected');
+                            return `
+                                <div class="${classes.join(' ')}" data-tweet="${tweetId}">
+                                    <div class="control">${tweetId}</div>
+                                    <div class="widget">
+                                    </div>
+                                </div>`;
+                        });
+                        let text = entries.join('');
+                        if (info.story)
+                            text = `<div class="story" data-story="${info.story}">${text}</div>`;
 
                         base.showSidebar('show-tweet')
                             .setContent(text);
 
-                        $('#sidebar .tweet').each((i, tweetTemplate) => {
-                            window.twttr.widgets.createTweet($(tweetTemplate).data('tweet'), tweetTemplate, {conversation: 'none'});
+                        $('#show-tweet-sidebar .tweet').each((i, e) => {
+                            let te = $(e)
+                            window.twttr.widgets.createTweet(te.data('tweet'), te.find('.widget')[0], {conversation: 'none'}).then(() => {
+                                te.addClass('loaded');
+                            });
                         });
-
-
-                        //window.twttr.widgets.createTweet(info.id, document.getElementById('tweetABC-' + info.id), {conversation: 'none'}).then(() => {
-                        //     console.debug('created tweet');
-                        // });
                     }
 
                 })

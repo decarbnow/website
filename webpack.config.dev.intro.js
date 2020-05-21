1c4aa1c06369e1252f6eb571061ab31da875f53c
petres <peter.reschenhofer@gmail.com>
2020-05-21 22:44:16 +0200

    use subdomains, renaming start to intro

---
+++ webpack.config.dev.intro.js
@@ -0,0 +1,15 @@
+const baseConfig = require('./webpack.config.base.js');
+const merge = require('webpack-merge');
+
+module.exports = merge(baseConfig, {
+    mode: 'development',
+
+    devServer: {
+        index: 'intro.html',
+        host: 'www.localhost',
+        disableHostCheck: true,
+        open: true,
+        liveReload: true
+    },
+    devtool: 'source-map'
+});

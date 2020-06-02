621f3df9b4b7a1266cd56b4bc067e737f0a191ab
sweing <sjweingaertner@gmail.com>
2020-06-02 17:09:39 +0200

    api java backend mapping

--- js/map/api/java.js
+++ js/map/api/java.js
@@ -3,13 +3,27 @@ export default function (data) {
     console.log('RECODE JAVA BACKEND OUTPUT')
     console.log(data)
 
-    let finalArray = [];
+
     let ltweets = [];
 
 
     function receiveData(data) {
         ltweets = data._embedded.poi.filter(filterData).map(mapToInternalStructure);
-        return getTweetArray(createDataTree(ltweets));
+
+        let tweetArray = getTweetArray(createDataTree(ltweets))
+        let finalArray = [];
+        for (var i in tweetArray) {
+            for (var j in tweetArray[i]) {
+                let name = tweetArray[i][j].tweetId
+                finalArray[name] = {
+                    'hashtags': tweetArray[i][j].hashtags,
+                    'story': i,
+                    'url': tweetArray[i][j].url
+                }
+            }
+        }
+        return(finalArray)
+
     }
 
     function filterData(item) {
@@ -17,26 +31,11 @@ export default function (data) {
     }
 
     function mapToInternalStructure(item) {
-      let tweetId = item.tweetId
-
-      let sp_hash = item.expandedUrl.indexOf('/@') + 2;
-      let ep_hash = item.expandedUrl.indexOf('/',sp_hash);
-
-      //get zoomLevel
-      let sp_z = item.expandedUrl.indexOf('/z=') + 3;
-      let ep_z = item.expandedUrl.indexOf('/',sp_z);
-
-      //get layers
-      let sp_ls = item.expandedUrl.indexOf('/ls=') + 4;
-      let ep_ls = item.expandedUrl.length;
-
-      return { 'tweetId': tweetId,
-                    '@': item.expandedUrl.substring(sp_hash,ep_hash),
-                    'z': item.expandedUrl.substring(sp_z,ep_z),
-                    'parentTweetId': item.inReplyToTweetId,
-                    'type': item.type,
-                    'replyFromSameUser': item.replyFromSameUser,
-                    'ls': item.expandedUrl.substring(sp_ls,ep_ls).split(",")}
+      return { 'tweetId': item.tweetId,
+                'url': item.expandedUrl,
+                'hashtags': item.type,
+                'parentTweetId': item.inReplyToTweetId,
+                'replyFromSameUser': item.replyFromSameUser}
     };
 
     const createDataTree = dataset => {
@@ -61,37 +60,40 @@ export default function (data) {
     }
 
     //GET ARRAY
-    // getTweetArray = function(data){
-    //   data.forEach((item, i) => {
-    //       let nameEntry = data[i].tweetId
-    //       finalArray[nameEntry] =  [];
-    //       flattenTrees(data[i],
-    //           function(node){ // do what you like with each node
-    //               finalArray[nameEntry].push(node);
-    //           }
-    //       );
-    //   });
-    // }
-
-
-    return {
-        "1264134811934961664": {
-            "hashtags": [],
-            "url": "/@u2838fkfw/z=11/ls=light,no2_2020_03,power-plants,tweets"
-        },
-        "1264135178450096134": {
-            "hashtags": [
-                "thisisatest"
-            ],
-            "story": "1264135178450096134",
-            "url": "/@u0y13ckzz/z=10/ls=light,power-plants"
-        },
-        "1264836376966713346": {
-            "hashtags": [
-                "buildingathread"
-            ],
-            "story": "1264135178450096134",
-            "url": "/@ezjqncs7z/z=10/ls=satellite,e-prtr,e-prtr2,power-plants,big-cities,tweets"
-        }
+    let getTweetArray = function(data){
+      let finalArray = [];
+      data.forEach((item, i) => {
+          let nameEntry = data[i].tweetId
+          finalArray[nameEntry] =  [];
+          flattenTrees(data[i],
+              function(node){ // do what you like with each node
+                  finalArray[nameEntry].push(node);
+              }
+          );
+      });
+      return(finalArray);
     }
+
+    return(receiveData(data))
+
+    // return {
+    //     "1264134811934961664": {
+    //         "hashtags": [],
+    //         "url": "/@u2838fkfw/z=11/ls=light,no2_2020_03,power-plants,tweets"
+    //     },
+    //     "1264135178450096134": {
+    //         "hashtags": [
+    //             "thisisatest"
+    //         ],
+    //         "story": "1264135178450096134",
+    //         "url": "/@u0y13ckzz/z=10/ls=light,power-plants"
+    //     },
+    //     "1264836376966713346": {
+    //         "hashtags": [
+    //             "buildingathread"
+    //         ],
+    //         "story": "1264135178450096134",
+    //         "url": "/@ezjqncs7z/z=10/ls=satellite,e-prtr,e-prtr2,power-plants,big-cities,tweets"
+    //     }
+    // }
 };
